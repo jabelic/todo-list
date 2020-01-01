bb44d4871e6e4d2df2718892caf508442803873e
Hibiki Mizuno <hibiki990523@gmail.com>
2020-01-01 22:32:18 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 
-const addTask = document.querySelector(".add");
-const list = document.querySelector(".task");
-
+function submitItem(){
+    var tasks = [];
+    var task = document.getElementById('task').value;
+    tasks.push(task);
+    for(var i=0; i< tasks.length; i++){
+        var li = document.createElement("li");
+        li.textContent = tasks[i];
+        document.getElementById("todo-list").appendChild(li);
+    }
+}
 
 
 

702de3f486a217e11b01827048fe31377ac34b92
Hibiki Mizuno <hibiki990523@gmail.com>
2020-01-02 00:00:19 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 
+
 function submitItem(){
     var tasks = [];
-    tasks.push(document.getElementById('task').value);
-    for(var i=0; i< tasks.length; i++){
-        var li = document.createElement("li");
-        li.textContent = tasks[i];
-        document.getElementById("todo-list").appendChild(li);
+    var task = document.getElementById('task');
+    var myButton = document.getElementById("submit-button");
+
+    myButton.onclick = function() {
+        var taskContent = task.value;
+        tasks.push(taskContent);
+        for(var i=0; i< tasks.length; i++){
+            var li = document.createElement("li");
+            li.textContent = tasks[i];
+            document.getElementById("todo-list").appendChild(li);
+        }
     }
 }
 
-document.addEventListener('readystatechange', function() {
-    if (document.readyState === "complete") {
-      submitItem();
+addTask.addEventListener('submit', e => {
+    // デフォルトのイベントを無効
+    e.preventDefault();
+
+    // タスクに入力した値を空白を除外して格納
+    const todo = addTask.add.value.trim();
+    if(todo.length) {
+        // Todo List の HTML を作成
+        createTodoList(todo);
+        // タスクに入力した文字をクリア
+        addTask.reset();
     }
 });
-
 
 

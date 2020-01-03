e62b09f42a56bc6a784356d1c2ad7348c70bb2ba
Hibiki Mizuno <hibiki990523@gmail.com>
2020-01-04 00:17:16 +0900

    update...

--- app.js
+++ app.js
@@ -3,10 +3,37 @@
 function submitItem(){
     var tasks = [];
     var task = document.getElementById('task');
+    //var myButton = document.getElementById("submit-button");
+    
+    var taskContent = task.value;
+        console.log(`入力欄の値: ${taskContent}`);
+        tasks.push(taskContent);
+        for(var i=0; i< tasks.length; i++){
+            var li = document.createElement("li");
+            li.textContent = tasks[i];
+            document.getElementById("todo-list").appendChild(li);
+        }
+    /*myButton.onclick = function() {
+        var taskContent = task.value;
+        console.log(`入力欄の値: ${taskContent}`);
+        tasks.push(taskContent);
+        for(var i=0; i< tasks.length; i++){
+            var li = document.createElement("li");
+            li.textContent = tasks[i];
+            document.getElementById("todo-list").appendChild(li);
+        }
+    }*/
+    
+}
+/*
+window.onload = function(){
+    var tasks = [];
+    var task = document.getElementById("task");
     var myButton = document.getElementById("submit-button");
 
     myButton.onclick = function() {
         var taskContent = task.value;
+        console.log(`入力欄の値: ${taskContent}`);
         tasks.push(taskContent);
         for(var i=0; i< tasks.length; i++){
             var li = document.createElement("li");
@@ -14,20 +41,17 @@ function submitItem(){
             document.getElementById("todo-list").appendChild(li);
         }
     }
-}
-
-addTask.addEventListener('submit', e => {
-    // デフォルトのイベントを無効
-    e.preventDefault();
-
-    // タスクに入力した値を空白を除外して格納
-    const todo = addTask.add.value.trim();
-    if(todo.length) {
-        // Todo List の HTML を作成
-        createTodoList(todo);
-        // タスクに入力した文字をクリア
-        addTask.reset();
-    }
-});
-
-
+};*/
+/* task.addEventListener('submit', e => {
+        // デフォルトのイベントを無効
+        e.preventDefault();
+    
+        // タスクに入力した値を空白を除外して格納
+        const todo = addTask.add.value.trim();
+        if(todo.length) {
+            // Todo List の HTML を作成
+            createTodoList(todo);
+            // タスクに入力した文字をクリア
+            addTask.reset();
+        }
+    });*/

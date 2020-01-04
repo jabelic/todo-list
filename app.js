0342d371824c623f71a09f9cd78715d06795a2b1
Hibiki Mizuno <hibiki990523@gmail.com>
2020-01-04 16:53:09 +0900

    repair the bug:0xE38080 -> 0x20

--- app.js
+++ app.js
@@ -1,51 +1,45 @@
 
 
-function submitItem(){
+function submitItem() {
     var tasks = [];
     var task = document.getElementById('task');
-    //var myButton = document.getElementById("submit-button");
-    
-    var taskContent = task.value;
-        console.log(`入力欄の値: ${taskContent}`);
-        tasks.push(taskContent);
-        for(var i=0; i< tasks.length; i++){
-            var li = document.createElement("li");
-            li.textContent = tasks[i];
-            document.getElementById("todo-list").appendChild(li);
-        }
-    /*myButton.onclick = function() {
-        var taskContent = task.value;
-        console.log(`入力欄の値: ${taskContent}`);
-        tasks.push(taskContent);
-        for(var i=0; i< tasks.length; i++){
-            var li = document.createElement("li");
-            li.textContent = tasks[i];
-            document.getElementById("todo-list").appendChild(li);
-        }
-    }*/
-    
-}
-/*
-window.onload = function(){
-    var tasks = [];
-    var task = document.getElementById("task");
-    var myButton = document.getElementById("submit-button");
 
-    myButton.onclick = function() {
-        var taskContent = task.value;
-        console.log(`入力欄の値: ${taskContent}`);
-        tasks.push(taskContent);
-        for(var i=0; i< tasks.length; i++){
-            var li = document.createElement("li");
-            li.textContent = tasks[i];
-            document.getElementById("todo-list").appendChild(li);
-        }
+    var taskContent = task.value;
+
+
+    console.log(`入力欄の値: ${taskContent}`);
+    tasks.push(taskContent);
+    for (var j = 0; j < tasks.length; j++) {
+        var li = document.createElement("li");
+        li.textContent = tasks[i];
+        document.getElementById("todo-list").appendChild(li);
+        var i = document.createElement("i");
+        i.setAttribute("class", "far fa-trash-alt delete");
+        document.getElementById("li").appendChild(i);
     }
-};*/
+
+}
+
+
+function reset() {
+
+    var reset_target = document.getElementById("text");
+
+    if (reset_target.value == '') {
+        alert('すでにリセットされています。');
+    } else {
+        reset_target.value = '';
+    }
+};
+
+
+
+
+
 /* task.addEventListener('submit', e => {
         // デフォルトのイベントを無効
         e.preventDefault();
-    
+
         // タスクに入力した値を空白を除外して格納
         const todo = addTask.add.value.trim();
         if(todo.length) {
@@ -55,3 +49,10 @@ window.onload = function(){
             addTask.reset();
         }
     });*/
+
+
+/*document.addEventListener('readystatechange', function() {
+    if (document.readyState === "complete") {
+        submitItem();
+    }
+});*/

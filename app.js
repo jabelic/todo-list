797f218cb7b8e8bf0036cfc8a7c4566f598634b7
Hibiki Mizuno <hibiki990523@gmail.com>
2020-02-24 17:11:22 +0900

    ??

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
 
-
-var newTag = function(tag){
-    var elem = document.createElement(tag);
-    for (var i=0; i<arguments.length; i++){
-        elem.appendChild(arguments[i]);
-    }
-    return elem;
-};
-
-var newText = function (text) {
-    return document.createTextNode(text);
-};
-
-
 function submitItem() {
     var tasks = [];
     var task = document.getElementById('task');
@@ -22,28 +8,26 @@ function submitItem() {
     tasks.push(taskContent);
     for (var j = 0; j < tasks.length; j++) {
         const li = document.createElement("li");
-        li.classList.add("list-group-item");
-        li.classList.add("d-flex");
-        li.classList.add("justify-content-between");
-        li.classList.add("align-items-center");
+        //li.classList.add("list-group-item");
+        //li.classList.add("d-flex");
+        //li.classList.add("justify-content-between");
+        //li.classList.add("align-items-center");
         li.textContent = tasks[j];
+        var input = document.createElement("input");
+        input.setAttribute("type","checkbox");
+        input.setAttribute("name","check");
+        input.classList.add("btn");
+        li.appendChild(input);
+        //li.insertBefore(input);
+
         var i = document.createElement("i");
         i.classList.add("far");
         i.classList.add("fa-trash-alt");
         i.classList.add("del");
         li.appendChild(i);
         document.getElementById("todo-list").appendChild(li);
-        /*
-        var i = document.createElement("i");
-        i.classList.add("far");
-        i.classList.add("fa-trash-alt");
-        i.classList.add("del");
-        li.appendChild(i);*/
     }
 }
-
-
-
 
 function reset() {
     var reset_target = document.getElementById("text");
@@ -55,50 +39,39 @@ function reset() {
     }
 };
 
-// 削除ボタンが押されたら発動
-function remove(){
-    var id = this.getAttribute('id');
-    todos.splice(id, 1);
-    show();
-}
 
-/*
-const addTask = document.querySelector('.add');
 const list = document.querySelector('.todos');
-
-const createTodoList = task => {
-    // HTML テンプレートを生成
-    const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${task}</span>
-        <i class="far fa-trash-alt delete"></i>
-    </li>
-    `;
-
-    list.innerHTML += html;
-}*/
-
-
-
-
-
-/* task.addEventListener('submit', e => {
-        // デフォルトのイベントを無効
-        e.preventDefault();
-
-        // タスクに入力した値を空白を除外して格納
-        const todo = addTask.add.value.trim();
-        if(todo.length) {
-            // Todo List の HTML を作成
-            createTodoList(todo);
-            // タスクに入力した文字をクリア
-            addTask.reset();
-        }
-    });*/
-
-
-/*document.addEventListener('readystatechange', function() {
-    if (document.readyState === "complete") {
-        submitItem();
+// 削除機能
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('del')){
+        e.target.parentElement.remove();
     }
-});*/
+},false);
+
+
+
+//const btn = document.getElementsByClassName("btn");
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btn = document.querySelector('.btn');
+    btn.addEventListener("click", ev => {
+    if (ev.target.classList.contains("btn")){;
+        ev.target.classList.contains("btn").style.color = "red"
+    }
+},false);
+}, false);
+
+
+
+function delchecked(){
+    // チェックボックス要素をすべて取得する
+    var boxes = document.querySelector(".todos");
+    // チェックボックスの個数を取得する
+    var cnt = boxes.length;
+    for(var i=0; i<cnt; i++) {
+        if(boxes.item(i).checked) {
+            //boxes.item(i).checked.remove();
+            console.log("hoge");
+        }
+    }
+};
